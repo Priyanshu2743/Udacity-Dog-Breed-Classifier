c3f7fff685fcfe44123483269ed41398e01110a4
Priyanshu Mittal <124867297+Priyanshu2743@users.noreply.github.com>
2025-02-12 11:57:59 +0530

    Add files via upload

--- print_results.py
+++ print_results.py
@@ -1,20 +1,17 @@
 def print_results(results_dic, results_stats_dic, model, 
                   print_incorrect_dogs = False, print_incorrect_breed = False):
-    print("\n\n*** Results Summary for CNN Model Architecture",model.upper(), 
-          "***")
-    print("{:20}: {:3d}".format('N Images', results_stats_dic['n_images']))
-    print("{:20}: {:3d}".format('N Dog Images', results_stats_dic['n_dogs_img']))
-    print("{:20}: {:3d}".format('N Not-Dog Images', results_stats_dic['n_notdogs_img']))
-
-    # Prints summary statistics (percent)
-    print("summarry model run: ")
+                  
+    print("\n\n *** Results Summary ",model.upper(), "***")
+    print("{:20}: {:3d}".format('N-Images', results_stats_dic['n_images']))
+    print("{:20}: {:3d}".format('N-Dog-Images', results_stats_dic['n_dogs_img']))
+    print("{:20}: {:3d}".format('N-Not-Dog-Images', results_stats_dic['n_notdogs_img']))
+    print(" Summarry_Model_Run: ")
+    
     for key in results_stats_dic:
-        print("\n".join(["{}: {}".format(key, results_stats_dic[key]) for key in results_stats_dic if
-                         key.startswith('p')]))
-                
-    # IF print_incorrect_dogs is True AND there were images incorrectly classified as dogs or vice versa - print out these cases        
+        print("\n".join(["{}: {}".format(key, results_stats_dic[key]) for key in results_stats_dic if key.startswith('p')]))
+            
     if (print_incorrect_dogs and 
-        ( (results_stats_dic['n_correct_dogs'] + results_stats_dic['n_correct_notdogs'])
+        ((results_stats_dic['n_correct_dogs'] + results_stats_dic['n_correct_notdogs'])
           != results_stats_dic['n_images'] ) 
        ):
         print("\nINCORRECT Dog/NOT Dog Assignments:")     
@@ -23,13 +20,10 @@ def print_results(results_dic, results_stats_dic, model,
             (results_dic[key][3] == 0 and results_dic[key][4] == 1):
                 print("Real: {}   Classifier: {}".format(results_dic[key][0],
                                                          results_dic[key][1]))
-            
-
     if (print_incorrect_breed and 
         (results_stats_dic['n_correct_dogs'] != results_stats_dic['n_correct_breed']) 
        ):
         print("\nINCORRECT Dog Breed Assignment:")
-
         for key in results_dic:
             if ( sum(results_dic[key][3:]) == 2 and
                 results_dic[key][2] == 0 ):
